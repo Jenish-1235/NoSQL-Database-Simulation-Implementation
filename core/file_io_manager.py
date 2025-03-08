00fe27df1aaab35bd5c7f02662b1399740456401
Jenish Togadiya <jenishtogadiya549@gmail.com>
2025-03-09 05:14:02 +0530

    prog: implement abstraction for file opening and closing

--- core/file_io_manager.py
+++ core/file_io_manager.py
@@ -1,3 +1,46 @@
 """
 Abstraction to quickly handle file I/O throughout the database service
 """
+
+class FileIoManager:
+    def __init__(self, db):
+        self.wal_file = None
+        self.ssTable_file = None
+        self.lookup_map_file = None
+        self.db = db
+
+    def openWAL(self, wal_file):
+        self.wal_file = wal_file
+        wal_file = open(wal_file, "a+")
+
+    def closeWAL(self):
+        if self.wal_file is not None:
+            self.wal_file.close()
+            self.wal_file = None
+        else:
+            print("No wal file opened")
+
+    def openSS(self, ssTable_file):
+        self.ssTable_file = ssTable_file
+        ssTable_file = open(ssTable_file, "a+")
+
+    def closeSS(self):
+        if self.ssTable_file is not None:
+            self.ssTable_file.close()
+            self.ssTable_file = None
+        else:
+            print("No ssTable file opened")
+
+    def openLookupMap(self, lookup_map_file):
+        self.lookup_map_file = lookup_map_file
+        lookup_map_file = open(lookup_map_file, "a+")
+
+    def closeLookupMap(self):
+        if self.lookup_map_file is not None:
+            self.lookup_map_file.close()
+            self.lookup_map_file = None
+        else:
+            print("No lookupMap file opened")
+
+
+
