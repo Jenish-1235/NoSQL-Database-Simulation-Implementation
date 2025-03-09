8a27c63c37f8f1c55d4bb7a7ebd3eb7a3207151d
Jenish Togadiya <jenishtogadiya549@gmail.com>
2025-03-09 05:49:50 +0530

    feat: implement read and write to memTable

--- api/v1/endpoints/database.py
+++ api/v1/endpoints/database.py
@@ -1,16 +1,39 @@
+from typing import Any, Dict
+
 from fastapi import APIRouter
+from fastapi.params import Depends
+import schemas.data
+
+from services.database_service import DatabaseService
 
 router = APIRouter()
+
+database_service = DatabaseService()
+
+def get_database_service():
+    return database_service
 
 @router.get("/")
 async def root():
     return {"message": "Welcome to NoSQL Database"}
 
 
-@router.get("/write")
-async def write():
-    return {"message": "Writing to database"}
+@router.post("/write")
+async def write(data: Dict[str, Any], database_service:DatabaseService = Depends(get_database_service)):
+    try:
+        key = data["key"]
+        value = data["value"]
+        await database_service.write_memTable(key, value)
+        return {"message": "Success"}
+    except Exception as e:
+        return {"message": str(e)}
 
 @router.get("/read")
-async def read():
-    return {"message": "Reading from database"}
+async def read(data: Dict[str, str], database_service:DatabaseService = Depends(get_database_service)):
+    try:
+        key = data["key"]
+        value = await database_service.read_memTable(key)
+        data = {"key": key, "value": value}
+        return data
+    except Exception as e:
+        return {"message": str(e)}
--- schemas/data.py
+++ schemas/data.py
@@ -1,3 +1,9 @@
 """
 Schema for data :- Key-Value pairs coming up from Users
 """
+from typing import Any
+
+
+class Data:
+    key: str
+    value: Any
