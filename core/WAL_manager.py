8248792173d60dffbd681c448355eddd31208c98
Jenish Togadiya <jenishtogadiya549@gmail.com>
2025-03-09 06:27:15 +0530

    feat: implement WAL update for write requests

--- core/WAL_manager.py
+++ core/WAL_manager.py
@@ -1,3 +1,18 @@
 """
 Class responsible to manage the WAL (Write Ahead Log) for whole database service
 """
+import datetime
+
+from core import file_io_manager
+from typing import Any, Dict
+
+
+class WALManager:
+    def __init__(self):
+        self.file_io_manager = file_io_manager.FileIOManager()
+        self.wal_file = "D:\\HLD-Assignments\\NoSQL-Database-Simulation-Implementation\\persistent-storage\\wal_file.txt"
+
+    async def update_wal_file(self, wal_data: Dict[str, Any], operation:str):
+        wal_record:str = "\n" + str(datetime.datetime.now()) +  " " + operation + " " + wal_data["key"] + " " + wal_data["value"]
+        self.file_io_manager.writeWAL(wal_file=self.wal_file, wal_record=wal_record)
+        return "Successfully updated WAL"
